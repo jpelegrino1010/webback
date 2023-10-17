c1b3d6ba239baf608c44bffd34fd8c703fcb14b9
jpelegrino1010 <julio.pelegrino@viseo.com>
2023-10-17 10:04:50 -0400

    add css loaders

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
-const App = () => <h1>Hello World from React</h1>;
+const App = () => (
+  <div className="container">
+    <h1>Hello World from React</h1>
+  </div>
+);
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+
+const javascriptRules = {
+  test: /\.js$/,
+  loader: "babel-loader",
+  options: {
+    presets: [
+      [
+        "@babel/preset-react",
+        {
+          runtime: "automatic",
+        },
+      ],
+    ],
+  },
+};
+
+const cssRules = {
+  test: /\.css$/,
+  use: ["style-loader", "css-loader"],
+};
+
+const rules = [javascriptRules, cssRules];
 
 module.exports = {
   //   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        options: {
-          presets: [
-            [
-              "@babel/preset-react",
-              {
-                runtime: "automatic",
-              },
-            ],
-          ],
-        },
-      },
-    ],
-  },
+  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+  module: { rules },
 };
